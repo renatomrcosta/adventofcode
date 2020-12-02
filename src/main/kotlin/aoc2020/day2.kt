860cd788423e927b5f42695c08e9b8ba0f541bec
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-02 20:38:40 +0100

    refactored structure based on day 2 specs

--- src/main/kotlin/aoc2020/day2.kt
+++ src/main/kotlin/aoc2020/day2.kt
@@ -2,45 +2,61 @@ package aoc2020
 
 // exemplifies the task at hand
 private val testInputs = listOf(
-    // "1-3 a: abcde",
-    // "1-3 b: cdefg",
-    // "2-9 c: ccccccccc",
-    "8-11 l: qllllqllklhlvtl",
-    "15-17 w: wwwwrswthgwhkwwrw"
+    "1-3 a: abcde",
+    "1-3 b: cdefg",
+    "2-9 c: ccccccccc",
 )
 
-// Small data representation of a password policy. It must be initialized with its necessary parameters
-private data class PasswordPolicy(
-    val password: String,
-    val letter: String,
+// generic data representation of a password policy.
+// the indices are different in the policies, so I only enforce the valid function and the password and letter parameters
+private sealed class GenericPasswordPolicy {
+    abstract val password: String
+    abstract val letter: String
+    abstract fun isPasswordValid(): Boolean
+
+}
+
+private data class TobogganPasswordPolicy(
+    override val password: String,
+    override val letter: String,
+    val initialIndex: Int,
+    val finalIndex: Int,
+) : GenericPasswordPolicy() {
+    override fun isPasswordValid(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
+
+private data class SledRentalPasswordPolicy(
+    override val password: String,
+    override val letter: String,
     val minLetterOccurrences: Int,
     val maxLetterOccurrences: Int,
-) {
+) : GenericPasswordPolicy() {
     init {
         check(minLetterOccurrences <= maxLetterOccurrences) { "minimum letter occurrences ($minLetterOccurrences) cannot be smaller than maximum number of letter occurences ($maxLetterOccurrences) " }
     }
 
-    // Checks the password string for the number of occurrences of a given string. Could likely be also done with regex.
-    val valid: Boolean by lazy {
+    override fun isPasswordValid(): Boolean {
         val letterCountMap = password.groupingBy { it.toString() }.eachCount()
-        val targetLetterCount = letterCountMap[letter] ?: return@lazy false // letter not found in policy
-        targetLetterCount in minLetterOccurrences..maxLetterOccurrences
+        val targetLetterCount = letterCountMap[letter] ?: return false // letter not found in policy
+        return targetLetterCount in minLetterOccurrences..maxLetterOccurrences
     }
 }
 
 fun part1() {
     println("Part1 - Test Data")
     testInputs
-        .map { it.mapToPasswordWithPolicy() }
-        .filter { it.valid }
+        .map { it.mapToPasswordWithPolicy<SledRentalPasswordPolicy>() }
+        .filter { it.isPasswordValid() }
         .run {
             println("Total: ${this.size} | Valid passwords: $this")
         }
 
     println("Part1 - Real Data")
     getInputFromFile()
-        .map { it.mapToPasswordWithPolicy() }
-        .filter { it.valid }
+        .map { it.mapToPasswordWithPolicy<SledRentalPasswordPolicy>() }
+        .filter { it.isPasswordValid() }
         .run {
             println("Total: ${this.size} | Valid passwords: $this")
         }
@@ -55,17 +71,26 @@ private fun getInputFromFile(): List<String> =
 private val numberMatcherRegex = Regex("[0-9]+")
 
 // Maps a string of the input format to a policy object
-private fun String.mapToPasswordWithPolicy(): PasswordPolicy {
+private inline fun <reified T : GenericPasswordPolicy> String.mapToPasswordWithPolicy(): T {
     val (policyString, password) = this.split(":")
     val letter = policyString.last().toString()
     val (minValue, maxValue) = numberMatcherRegex.findAll(policyString).toList()
 
-    return PasswordPolicy(
-        password = password.trim(),
-        letter = letter,
-        minLetterOccurrences = minValue.value.toInt(),
-        maxLetterOccurrences = maxValue.value.toInt(),
-    )
+    return when (T::class) {
+        TobogganPasswordPolicy::class -> TobogganPasswordPolicy(
+            password = password.trim(),
+            letter = letter,
+            initialIndex = minValue.value.toInt(),
+            finalIndex = maxValue.value.toInt(),
+        )
+        SledRentalPasswordPolicy::class -> SledRentalPasswordPolicy(
+            password = password.trim(),
+            letter = letter,
+            minLetterOccurrences = minValue.value.toInt(),
+            maxLetterOccurrences = maxValue.value.toInt(),
+        )
+        else -> error("Invalid policy type")
+    } as T
 }
 
 fun main() {
