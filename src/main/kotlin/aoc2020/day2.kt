cdf5456fbb4714d853f13d7d316b9901949b38fd
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-02 20:01:11 +0100

    day2 part1

--- src/main/kotlin/aoc2020/day2.kt
+++ src/main/kotlin/aoc2020/day2.kt
@@ -1,12 +1,17 @@
 package aoc2020
 
+// exemplifies the task at hand
 private val testInputs = listOf(
-    "1-3 a: abcde",
-    "1-3 b: cdefg",
-    "2-9 c: ccccccccc",
+    // "1-3 a: abcde",
+    // "1-3 b: cdefg",
+    // "2-9 c: ccccccccc",
+    "8-11 l: qllllqllklhlvtl",
+    "15-17 w: wwwwrswthgwhkwwrw"
 )
 
-private class Policy(
+// Small data representation of a password policy. It must be initialized with its necessary parameters
+private data class PasswordPolicy(
+    val password: String,
     val letter: String,
     val minLetterOccurrences: Int,
     val maxLetterOccurrences: Int,
@@ -15,33 +20,53 @@ private class Policy(
         check(minLetterOccurrences <= maxLetterOccurrences) { "minimum letter occurrences ($minLetterOccurrences) cannot be smaller than maximum number of letter occurences ($maxLetterOccurrences) " }
     }
 
-    fun isPasswordValid(password: String): Boolean = TODO()
+    // Checks the password string for the number of occurrences of a given string. Could likely be also done with regex.
+    val valid: Boolean by lazy {
+        val letterCountMap = password.groupingBy { it.toString() }.eachCount()
+        val targetLetterCount = letterCountMap[letter] ?: return@lazy false // letter not found in policy
+        targetLetterCount in minLetterOccurrences..maxLetterOccurrences
+    }
 }
 
 fun part1() {
     println("Part1 - Test Data")
-    val mappedTestInputs = testInputs
-        .map { it.mapToPassword() to it.mapToPolicy() }.toMap()
-    findValidPasswords(mappedTestInputs).run { println(this) }
+    testInputs
+        .map { it.mapToPasswordWithPolicy() }
+        .filter { it.valid }
+        .run {
+            println("Total: ${this.size} | Valid passwords: $this")
+        }
 
     println("Part1 - Real Data")
-    val mappedInputs = getInputFromFile()
-        .map { it.mapToPassword() to it.mapToPolicy() }.toMap()
-    findValidPasswords(mappedInputs).run { println(this) }
+    getInputFromFile()
+        .map { it.mapToPasswordWithPolicy() }
+        .filter { it.valid }
+        .run {
+            println("Total: ${this.size} | Valid passwords: $this")
+        }
 }
 
+// Reads the test input from file as a list of strings
 private fun getInputFromFile(): List<String> =
     readFile("day2.txt")
         .split("\n")
         .filter { it.isNotEmpty() }
 
-private fun findValidPasswords(inputs: Map<String, Policy>): List<String> = inputs.entries
-    .filter { (password, policy) ->
-        policy.isPasswordValid(password)
-    }.map { it.key }
+private val numberMatcherRegex = Regex("[0-9]+")
 
-private fun String.mapToPolicy(): Policy = TODO()
-private fun String.mapToPassword(): String = TODO()
+// Maps a string of the input format to a policy object
+private fun String.mapToPasswordWithPolicy(): PasswordPolicy {
+    val (policyString, password) = this.split(":")
+    val letter = policyString.last().toString()
+    val (minValue, maxValue) = numberMatcherRegex.findAll(policyString).toList()
+
+    return PasswordPolicy(
+        password = password.trim(),
+        letter = letter,
+        minLetterOccurrences = minValue.value.toInt(),
+        maxLetterOccurrences = maxValue.value.toInt(),
+    )
+}
 
 fun main() {
     part1()
