84c6abf469cdf3d0ac519d09359eb4639f65fa7f
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-06 14:31:40 +0100

    coroutined

--- src/main/kotlin/aoc2020/day6/day6.kt
+++ src/main/kotlin/aoc2020/day6/day6.kt
@@ -2,51 +2,74 @@ package aoc2020.day6
 
 import aoc2020.readFile
 import aoc2020.splitNewLines
+import aoc2020.withExecutionTime
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
-fun main() {
-    println("Part 1 - Test Data")
-    parseInput(testData).run {
-        val answerSum = sumOf { it.size }
-        println("Group Reply Count = $answerSum")
-        assert(answerSum == 11)
-    }
+fun main(): Unit = withExecutionTime {
+    runBlocking(Dispatchers.Default) {
+        launch {
+            println("Part 1 - Test Data")
+            parseInput(testData).run {
+                val answerSum = sumOf { it.size }
+                println("Group Reply Count = $answerSum")
+                assert(answerSum == 11)
+            }
+        }
 
-    println("Part 1 - Input file")
-    parseInput(readFile("day6.txt")).run {
-        println("Group Reply Count = " + sumOf { it.size })
-    }
+        launch {
+            println("Part 1 - Input file")
+            parseInput(readFile("day6.txt")).run {
+                println("Group Reply Count = " + sumOf { it.size })
+            }
+        }
 
-    println("Part2 - Test Data")
-    parseInputPart2(testData).run {
-        println("Test part 2 - $this")
-        val answerSum = sumOf { it.size }
-        println("Part2 test - Group Reply Count = $answerSum")
-        assert(answerSum == 6)
-    }
+        launch {
+            println("Part2 - Test Data")
+            parseInputPart2(testData).run {
+                println("Test part 2 - $this")
+                val answerSum = sumOf { it.size }
+                println("Part2 test - Group Reply Count = $answerSum")
+                assert(answerSum == 6)
+            }
+        }
 
-    println("Part 2 - Input file")
-    parseInputPart2(readFile("day6.txt")).run {
-        println("Part2 answer - Group Reply Count = " + sumBy { it.size })
+        launch {
+            println("Part 2 - Input file")
+            parseInputPart2(readFile("day6.txt")).run {
+                println("Part2 answer - Group Reply Count = " + sumBy { it.size })
+            }
+        }
     }
 }
 
-fun parseInput(input: String): List<Set<Char>> =
+suspend fun parseInput(input: String): List<Set<Char>> =
     input
         .splitNewLines()
+        .asFlow()
         .filter { it.isNotEmpty() }
         .map { it.replace("\n", "") }
         .map { it.toSet() }
+        .toList()
 
-fun parseInputPart2(input: String) = input.splitNewLines()
-    .filter { it.isNotEmpty() }
-    .map { it.trim() }
-    .map { it.split("\n") }
-    .map { set ->
-        set.map { it.toSet() }
-            .reduce { acc, s ->
-                acc intersect s
-            }
-    }.filter { it.isNotEmpty() }
+suspend fun parseInputPart2(input: String): List<Set<Char>> =
+    input.splitNewLines()
+        .asFlow()
+        .filter { it.isNotEmpty() }
+        .map { it.trim() }
+        .map { it.split("\n") }
+        .map { set ->
+            set.map { it.toSet() }
+                .reduce { acc, s ->
+                    acc intersect s
+                }
+        }.filter { it.isNotEmpty() }
+        .toList()
 
 val testData = """
     abc
