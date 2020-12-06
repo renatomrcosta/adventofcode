dc421bf26750baeecdef90d7cd2f5a6d179f1a75
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-06 11:01:19 +0100

    wip part2

--- src/main/kotlin/aoc2020/day6/day6.kt
+++ src/main/kotlin/aoc2020/day6/day6.kt
@@ -15,16 +15,40 @@ fun main() {
     parseInput(readFile("day6.txt")).run {
         println("Group Reply Count = " + sumOf { it.size })
     }
+
+    println("Part2 - Test Data")
+    parseInputPart2(testData).run {
+        println("Test part 2 - $this")
+        val answerSum = sumOf { it.size  }
+        println("Part2 test - Group Reply Count = $answerSum")
+        assert(answerSum == 6)
+    }
+
+    println("Part 2 - Input file")
+    parseInputPart2(readFile("day6.txt")).run {
+        println("Part2 answer - Group Reply Count = " + sumBy { it.size })
+    }
 }
 
-private fun parseInput(input: String): List<Set<Char>> =
+fun parseInput(input: String): List<Set<Char>> =
     input
         .splitNewLines()
         .filter { it.isNotEmpty() }
         .map { it.replace("\n", "") }
         .map { it.toSet() }
 
-private val testData = """
+fun parseInputPart2(input: String) = input.splitNewLines()
+    .filter { it.isNotEmpty() }
+    .map { it.split("\n") }
+    .map { it.toSet() }
+    .map { set ->
+        set.map { it.toSet() }
+            .reduce { acc, s ->
+                acc intersect s
+            }
+    }.filter { it.isNotEmpty() }
+
+val testData = """
     abc
 
     a
@@ -40,4 +64,33 @@ private val testData = """
     a
 
     b
+""".trimIndent()
+
+val testData2 = """
+    abc
+
+    a
+    b
+    c
+
+    ab
+    ac
+
+    a
+    ac
+    a
+    ad
+
+    b
+    
+    abc
+    c
+    ac
+    
+    xa
+    ya
+    za
+    xxa
+    bbbab
+    ca
 """.trimIndent()
