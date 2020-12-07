b1c07d4291907c7ba8f07869bbbcce71dcf9812d
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-07 15:40:32 +0100

    day2

--- src/main/kotlin/aoc2020/day7/day7.kt
+++ src/main/kotlin/aoc2020/day7/day7.kt
@@ -3,7 +3,39 @@ package aoc2020.day7
 import aoc2020.readFile
 
 fun main() {
-    part1()
+    // part1()
+    part2()
+}
+
+private fun part2() {
+    println("Part 2 - Test Data")
+    val bagInput = "shiny gold bag"
+    parseInput(testData).run {
+        val result = getChildrenAmount(bagInput, this).also { println(it) }
+        assert(result - 1 == 32)
+    }
+    parseInput(testData2).run {
+        val result = getChildrenAmount(bagInput, this).also { println(it) }
+        assert(result - 1 == 126)
+    }
+
+    println("Part 2 - Input Data")
+    parseInput(readFile("day7.txt")).run {
+        getChildrenAmount(bagInput, this).also { println(it-1) }
+    }
+}
+
+fun getChildrenAmount(
+    bag: String,
+    input: Map<String, List<BagRule>>,
+): Int {
+    val entry = input[bag] ?: error("no rule defined for key $bag")
+
+    if (entry.isEmpty()) return 1
+
+    return entry.map {
+        getChildrenAmount(it.heldBag, input) * it.maxQuantity
+    }.sum() + 1
 }
 
 private fun part1() {
@@ -84,5 +116,16 @@ val testData = """
     vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
     faded blue bags contain no other bags.
     dotted black bags contain no other bags.
+    
+""".trimIndent()
+
+val testData2 = """
+    shiny gold bags contain 2 dark red bags.
+    dark red bags contain 2 dark orange bags.
+    dark orange bags contain 2 dark yellow bags.
+    dark yellow bags contain 2 dark green bags.
+    dark green bags contain 2 dark blue bags.
+    dark blue bags contain 2 dark violet bags.
+    dark violet bags contain no other bags.
     
 """.trimIndent()
