ba59fac629afc9110cadcd671922c89dd16625b9
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-09 16:13:52 +0100

    small fixes

--- src/main/kotlin/aoc2020/day9/day9.kt
+++ src/main/kotlin/aoc2020/day9/day9.kt
@@ -12,10 +12,10 @@ fun part1() {
     parseInput(testData).run {
         val outlier = findOutlier(this, 5).also { println("Outlier is $it") }
         println("Part2 - Test Data")
+
         findContiguousSum(this, outlier)
-            .filter { it.size > 1 }
-            .map { getResult(it) }
-            .forEach { println(it) }
+            .getResult()
+            .also { println("Result = $it") }
 
     }
 
@@ -24,10 +24,8 @@ fun part1() {
         val outlier = findOutlier(this, 25).also { println("Outlier is $it") }
         println("Part2 - Input Data")
         findContiguousSum(this, outlier)
-            .filter { it.size > 1 }
-            .also { println(it) }
-            .map { getResult(it) }
-            .forEach { println(it) }
+            .getResult()
+            .also { println("Result = $it") }
     }
 }
 
@@ -48,29 +46,23 @@ fun hasSumInSlice(slice: List<Long>, target: Long): Boolean {
     return false
 }
 
-fun findContiguousSum(input: List<Long>, target: Long): List<List<Long>> {
-    // Consider that more than one combination might exist?
-    val results = mutableListOf<List<Long>>()
+fun findContiguousSum(input: List<Long>, target: Long): List<Long> {
     for (index in input.indices - 1) {
         input
             .drop(index)
             .runningFoldIndexed(0L) { foldIndex, acc, item ->
                 val sum = acc + item
                 if (sum == target) {
-                    results.add(input.subList(index, (foldIndex + index) + 1))
-                } else if (sum > target) {
-                    // Break the fold???
+                    return input.subList(index, (foldIndex + index) + 1)
                 }
                 sum
             }
     }
-    return results
+    error("no results found")
 }
 
-fun getResult(contiguousSumList: List<Long>): Long {
-    val min = contiguousSumList.minOrNull() ?: error("something funky. No min value")
-    val max = contiguousSumList.maxOrNull() ?: error("something funky. No max value")
-    return min + max
+fun List<Long>.getResult(): Long = this.sorted().let { sorted ->
+    sorted.first() + sorted.last()
 }
 
 fun parseInput(input: String): List<Long> =
