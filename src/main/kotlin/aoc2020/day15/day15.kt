af54f3ce9c0dd5d64884eedc57ce71334a046af6
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-15 12:46:19 +0100

    day15 part2 - with index mapping.

--- src/main/kotlin/aoc2020/day15/day15.kt
+++ src/main/kotlin/aoc2020/day15/day15.kt
@@ -1,33 +1,36 @@
 package aoc2020.day15
 
-import kotlinx.coroutines.Dispatchers
+import aoc2020.withExecutionTime
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.runBlocking
 
 fun main() {
-    runBlocking {
-        // Game(testData).play(2020).also { println(it.last()) }
-        // Game(input).play(2020).also { println(it.last()) }
-        //
-        // Game(testData).play(30_000_000).also { println(it.last()) }
-        Game(input).play(30_000_000).also { println(it.last()) }
+    withExecutionTime {
+        runBlocking {
+            Game(testData).play(2020).also { println(it.last()) }
+            Game(input).play(2020).also { println(it.last()) }
+
+            Game(testData).play(30_000_000).also { println(it.last()) }
+            Game(input).play(30_000_000).also { println(it.last()) }
+        }
     }
 }
 
 data class Game(val input: List<Int>) {
     private var inputTurnCount = input.size
-    private var numbers = mutableListOf(*input.reversed().toTypedArray(), -10)
+    private val numbers = mutableListOf(-10, *input.toTypedArray())
+    private val lastIndexMap = numbers.dropLast(1).mapIndexed { index, item -> item to index }.toMap().toMutableMap()
 
     private val flow = flow {
         while (true) {
-            val currentItem = numbers.first()
-            val itemIndex = numbers.drop(1).indexOf(currentItem)
+            val currentItem = numbers.last()
+            val itemIndex = lastIndexMap.getOrDefault(currentItem, -1)
+            lastIndexMap[currentItem] = numbers.lastIndex
 
             if (itemIndex >= 0) {
-                emit(itemIndex + 1)
+                emit(numbers.lastIndex - itemIndex)
             } else {
                 emit(0)
             }
@@ -38,11 +41,10 @@ data class Game(val input: List<Int>) {
         flow
             .take(turns - inputTurnCount)
             .collect {
-                numbers.add(0, it)
-                if (numbers.lastIndex % 100_000 == 0) println("${numbers.lastIndex} turns played")
+                numbers.add(it)
+                if (numbers.lastIndex % 1_000_000 == 0) println("${numbers.lastIndex} turns played")
             }
-
-        return numbers.reversed()
+        return numbers
     }
 }
 
