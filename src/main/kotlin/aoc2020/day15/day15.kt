e95e1b6a33d011b25796255a8ed08ff3f12b9250
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-15 12:52:52 +0100

    day15 part2 - simplified

--- src/main/kotlin/aoc2020/day15/day15.kt
+++ src/main/kotlin/aoc2020/day15/day15.kt
@@ -1,49 +1,38 @@
 package aoc2020.day15
 
 import aoc2020.withExecutionTime
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.runBlocking
 
 fun main() {
     withExecutionTime {
-        runBlocking {
-            Game(testData).play(2020).also { println(it.last()) }
-            Game(input).play(2020).also { println(it.last()) }
+        // Game(testData).play(2020).also { println(it.last()) }
+        // Game(input).play(2020).also { println(it.last()) }
 
-            Game(testData).play(30_000_000).also { println(it.last()) }
-            Game(input).play(30_000_000).also { println(it.last()) }
-        }
+        // Game(testData).play(30_000_000).also { println(it.last()) }
+        Game(input).play(30_000_000).also { println(it.last()) }
     }
 }
 
 data class Game(val input: List<Int>) {
-    private var inputTurnCount = input.size
-    private val numbers = mutableListOf(-10, *input.toTypedArray())
-    private val lastIndexMap = numbers.dropLast(1).mapIndexed { index, item -> item to index }.toMap().toMutableMap()
+    private val numbers = mutableListOf(-10, *input.toTypedArray()) // I initialize with an offset to ignore "Turn 0"
 
-    private val flow = flow {
-        while (true) {
+    private val lastIndexMap = numbers
+        .dropLast(1)
+        .mapIndexed { index, item -> item to index }.toMap()
+        .toMutableMap() // Index all but the first item processed }
+
+    fun play(turns: Int): List<Int> {
+        repeat((input.size until turns).count()) {
             val currentItem = numbers.last()
             val itemIndex = lastIndexMap.getOrDefault(currentItem, -1)
             lastIndexMap[currentItem] = numbers.lastIndex
 
             if (itemIndex >= 0) {
-                emit(numbers.lastIndex - itemIndex)
+                numbers.add(numbers.lastIndex - itemIndex)
             } else {
-                emit(0)
+                numbers.add(0)
             }
         }
-    }
 
-    suspend fun play(turns: Int): List<Int> {
-        flow
-            .take(turns - inputTurnCount)
-            .collect {
-                numbers.add(it)
-                if (numbers.lastIndex % 1_000_000 == 0) println("${numbers.lastIndex} turns played")
-            }
         return numbers
     }
 }
