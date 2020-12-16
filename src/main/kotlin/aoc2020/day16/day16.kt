05b1c760d757c947e4a200e5c82484844956ff06
Renato Costa <renato.rezende.costa@gmail.com>
2020-12-16 10:31:59 +0100

    part2

--- src/main/kotlin/aoc2020/day16/day16.kt
+++ src/main/kotlin/aoc2020/day16/day16.kt
@@ -5,17 +5,35 @@ import aoc2020.splitOnBlankLines
 import aoc2020.splitOnLineBreaks
 
 fun main() {
-    testData.toTicket().run {
-        findAllFullyInvalidItemsInNearbyTickets().run {
-            println("Invalids: $this")
-            println("Sum of invalids: ${this.sum()}")
-        }
+    // testData.toTicket().run {
+    // findAllFullyInvalidItemsInNearbyTickets().run {
+    //     println("Count: ${this.size} || Invalids: $this")
+    //     println("Sum of invalids: ${this.sum()}")
+    // }
+    // }
+    // readFile("day16.txt").toTicket().run {
+    //     findAllFullyInvalidItemsInNearbyTickets().run {
+    //         println("Count: ${this.size} || Invalids: $this")
+    //         println("Sum of invalids: ${this.sum()}")
+    //     }
+    // }
+
+    testData2.toTicket().cleanTicket().run {
+        val potentialIndices = mapConditionToIndices()
+        cleanIndicesUp(potentialIndices).also { println(it) }
     }
-    readFile("day16.txt").toTicket().run {
-        findAllFullyInvalidItemsInNearbyTickets().run {
-            println("Invalids: $this")
-            println("Sum of invalids: ${this.sum()}")
-        }
+
+    readFile("day16.txt").toTicket().cleanTicket().run {
+        val potentialIndices = mapConditionToIndices()
+        val cleanedUpIndices = cleanIndicesUp(potentialIndices).also { println(it) }
+        val results = cleanedUpIndices
+            .filter { it.key.startsWith("departure") }
+            .also { println(it) }
+            .map { (_, index) ->
+                ticketParameters[index].toLong()
+            }
+        println("Results = $results")
+        println("Multiplied results = ${results.reduce { acc, i -> acc * i }}")
     }
 }
 
@@ -24,9 +42,11 @@ data class Ticket(
     val ticketParameters: List<Int>,
     val nearbyTicketParameters: List<List<Int>>,
 ) {
-    fun findAllFullyInvalidItemsInNearbyTickets() =
-        this.nearbyTicketParameters.flatten().filter { !isInAnyValidCondition(it) }
 
+    private val flatConditions by lazy { this.nearbyTicketParameters.flatten() }
+
+    fun findAllFullyInvalidItemsInNearbyTickets() =
+        flatConditions.filter { !isInAnyValidCondition(it) }
 
     private fun isInAnyValidCondition(input: Int): Boolean {
         for (range in conditions.values.flatten()) {
@@ -34,6 +54,63 @@ data class Ticket(
         }
         return false
     }
+
+    fun cleanTicket(): Ticket {
+        val cleanNearbyTickets = nearbyTicketParameters.filter { list ->
+            list.forEach {
+                if (!isInAnyValidCondition(it)) {
+                    return@filter false
+                }
+            }
+            return@filter true
+        }
+
+        return this.copy(nearbyTicketParameters = cleanNearbyTickets)
+    }
+
+    fun cleanIndicesUp(input: Map<String, MutableList<Int>>): Map<String, Int> {
+        val result = mutableMapOf<String, Int>()
+        val mutableInput = input.toMutableMap()
+
+        while (mutableInput.isNotEmpty()) {
+            val (key, list) = mutableInput.entries.find { it.value.size == 1 } ?: error("No single index entry found")
+
+            val value = list.first()
+            result[key] = value
+
+            mutableInput.remove(key)
+            mutableInput.values.forEach { it.remove(value) }
+        }
+
+        return result
+    }
+
+    fun mapConditionToIndices(): Map<String, MutableList<Int>> {
+        val result = mutableMapOf<String, MutableList<Int>>()
+
+        conditions.forEach { (key, ranges) ->
+            // Match every condition to every index in our range of nearby tickets and pick whichever is 100% valid
+            var index = 0
+            itemLoop@ while (index <= nearbyTicketParameters[0].lastIndex) {
+                var isValid = true
+                tickets@ for (nearbyTicket in nearbyTicketParameters) {
+                    val item = nearbyTicket[index]
+                    isValid = validate(item, ranges)
+                    if (!isValid) break@tickets
+                }
+                if (isValid) {
+                    result.putIfAbsent(key, mutableListOf())
+                    result[key]?.add(index) ?: error("should never occur")
+                }
+                index++
+            }
+        }
+
+        return result
+    }
+
+    private fun validate(value: Int, ranges: List<IntRange>): Boolean =
+        ranges.any { it.contains(value) }
 }
 
 fun String.toTicket(): Ticket {
@@ -83,4 +160,19 @@ val testData = """
     40,4,50
     55,2,20
     38,6,12
+""".trimIndent()
+
+val testData2 = """
+    class: 0-1 or 4-19
+    row: 0-5 or 8-19
+    seat: 0-13 or 16-19
+
+    your ticket:
+    11,12,13
+
+    nearby tickets:
+    3,9,18
+    15,1,5
+    5,14,9
+    
 """.trimIndent()
