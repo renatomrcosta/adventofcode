3d52fd917f1ca0169e3b16ef5be8feb1ac2336ad
Renato Costa <renato.rezende.costa@gmail.com>
2021-12-06 20:01:21 +0100

    day6 part2

--- src/main/kotlin/aoc2021/day6/day6.kt
+++ src/main/kotlin/aoc2021/day6/day6.kt
@@ -1,9 +1,11 @@
 package aoc2021.day6
 
 import aoc2021.readFile
+import aoc2021.withExecutionTime
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.transform
+import java.util.*
 
 private val testInput = "3,4,3,1,2"
 
@@ -14,6 +16,16 @@ suspend fun main() {
     check(calculatePart1(testInput.parse()) == 5934)
     val part1 = calculatePart1(file.parse())
     println("Part1: $part1")
+
+    println("part2")
+    withExecutionTime {
+        check(calculatePart2(testInput.parseInt(), 80) == 5934L)
+        check(calculatePart2(testInput.parseInt(), 256) == 26984457539)
+    }
+    withExecutionTime {
+        val part2 = calculatePart2(file.parseInt(), 256)
+        println("Part2: $part2")
+    }
 }
 
 private suspend fun calculatePart1(input: List<Lanternfish>): Int {
@@ -29,11 +41,30 @@ private suspend fun calculatePart1(input: List<Lanternfish>): Int {
     return flow.count().also { println("Count=$it") }
 }
 
+private fun calculatePart2(input: List<Int>, days: Int): Long {
+    val buckets = MutableList(9) { 0L }
+
+    input.groupingBy { it }.eachCount().forEach { (index, count) ->
+        buckets[index] = count.toLong()
+    }
+    repeat(days) {
+        Collections.rotate(buckets, -1)
+        // every item on the bucket #8 is a new spawn, and the same amount should go to bucket 6 (parent reset)
+        buckets[6] += buckets[8]
+    }
+    return buckets.sum()
+}
+
 
 private fun String.parse() = this
     .splitToSequence(",")
     .map { it.toInt() }
     .map { Lanternfish(timer = it) }
+    .toList()
+
+private fun String.parseInt() = this
+    .splitToSequence(",")
+    .map { it.toInt() }
     .toList()
 
 private class Lanternfish(var timer: Int) {
