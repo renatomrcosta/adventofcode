eefd58fef1c3dc7102b4a403c6dd89cf7bec921d
Renato Costa <renato.rezende.costa@gmail.com>
2021-12-09 10:47:14 +0100

    day9 part2

--- src/main/kotlin/aoc2021/day9/day9.kt
+++ src/main/kotlin/aoc2021/day9/day9.kt
@@ -16,11 +16,39 @@ fun main() {
     println("Part1")
     calculatePart1(testInput.parse()).run { check(this == 15) }
     calculatePart1(file.parse()).run { println("Part1 Result: $this") }
+
+    println("Part2")
+    calculatePart2(testInput.parse()).run { check(this == 1134) }
+    calculatePart2(file.parse()).run { println("Part2 Result: $this") }
 }
 
 private fun calculatePart1(input: Array<IntArray>): Int {
     val lowPoints = getAllLowPoints(input)
     return lowPoints.sumOf { it + 1 }
+}
+
+private fun calculatePart2(input: Array<IntArray>): Int =
+    getAllLowPointsIndexed(input)
+        .map { (value, index) -> getBasinSize(input, index) }
+        .sortedDescending()
+        .take(3)
+        .reduce { acc, value -> acc * value }
+
+private fun getBasinSize(input: Array<IntArray>, index: Pair<Int, Int>): Int {
+    val adjacents = input
+        .getAdjacentOfIndexIndexed(index.first, index.second)
+        .filter { (value, _) -> value != null && value != 9 }
+
+    input[index.first][index.second] = 9
+
+    var count = 1
+    adjacents.forEach { (_, adjacentIndex) ->
+        input[adjacentIndex.first][adjacentIndex.second] = 9
+    }
+    adjacents.forEach { (_, adjacentIndex) ->
+        count += getBasinSize(input, adjacentIndex)
+    }
+    return count
 }
 
 private fun getAllLowPoints(input: Array<IntArray>): List<Int> = buildList {
@@ -34,6 +62,17 @@ private fun getAllLowPoints(input: Array<IntArray>): List<Int> = buildList {
     }
 }
 
+private fun getAllLowPointsIndexed(input: Array<IntArray>): List<Pair<Int, Pair<Int, Int>>> = buildList {
+    input.forEachIndexed { i, row ->
+        row.forEachIndexed { j, cell ->
+            val adjacents = input.getAdjacentOfIndex(i, j)
+            if (adjacents.all { it > cell }) {
+                add(cell to (i to j))
+            }
+        }
+    }
+}
+
 private fun Array<IntArray>.getAdjacentOfIndex(x: Int, y: Int): List<Int> {
     val left = this.getOrNull(x - 1)?.getOrNull(y)
     val right = this.getOrNull(x + 1)?.getOrNull(y)
@@ -41,6 +80,15 @@ private fun Array<IntArray>.getAdjacentOfIndex(x: Int, y: Int): List<Int> {
     val down = this.getOrNull(x)?.getOrNull(y + 1)
 
     return listOfNotNull(left, right, up, down)
+}
+
+private fun Array<IntArray>.getAdjacentOfIndexIndexed(x: Int, y: Int): List<Pair<Int?, Pair<Int, Int>>> {
+    val left = this.getOrNull(x - 1)?.getOrNull(y) to (x - 1 to y)
+    val right = this.getOrNull(x + 1)?.getOrNull(y) to (x + 1 to y)
+    val up = this.getOrNull(x)?.getOrNull(y - 1) to (x to y - 1)
+    val down = this.getOrNull(x)?.getOrNull(y + 1) to (x to y + 1)
+
+    return listOf(left, right, up, down)
 }
 
 private fun String.parse() = this.splitOnLineBreaks()
