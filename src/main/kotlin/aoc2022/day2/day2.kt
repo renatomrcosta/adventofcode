4a47b789e76f039f2befb1d5bc8f5ff24f196823
Renato Costa <renato@closelink.net>
2022-12-02 08:19:45 +0100

    2022-02 - part2

--- src/main/kotlin/aoc2022/day2/day2.kt
+++ src/main/kotlin/aoc2022/day2/day2.kt
@@ -11,17 +11,33 @@ val testData = """
 
 fun main() {
     part1()
+    part2()
 }
 
 private fun part1() {
-    testData.parseInput().sumOf { (opponent, player) -> player.play(opponent) }.run { require(this == 15) }
-    readFile("day2.txt").parseInput().sumOf { (opponent, player) -> player.play(opponent) }.run { println("Total: $this")}
+    testData.parseInputPart1().sumOf { (opponent, player) -> player.play(opponent) }.run { require(this == 15) }
+    readFile("day2.txt").parseInputPart1().sumOf { (opponent, player) -> player.play(opponent) }.run { println("Total: $this")}
 }
 
-private fun String.parseInput() =
+private fun part2() {
+    testData.parseInputPart2().sumOf { (opponent, player) -> player.play(opponent) }.run { require(this == 12) }
+    readFile("day2.txt").parseInputPart2().sumOf { (opponent, player) -> player.play(opponent) }.run { println("Total: $this")}
+}
+
+private fun String.parseInputPart1() =
     this.splitOnLineBreaks()
         .map { it.split(" ") }
         .map { it.first().toRPS() to it.last().toRPS() }
+
+private fun String.parseInputPart2() =
+    this.splitOnLineBreaks()
+        .map { it.split(" ") }
+        .map {
+            val opponent = it.first().toRPS()
+            val desiredMove =  it.last().toDesiredOutcome(opponent)
+            opponent to desiredMove
+        }
+
 
 private enum class RPS(private val score: Int) {
     Rock(score = 1), Paper(score = 2), Scissors(score = 3);
@@ -47,6 +63,24 @@ private enum class RPS(private val score: Int) {
             }
         }
         return outcome + this.score
+    }
+
+
+}
+private fun String.toDesiredOutcome(oppenent: RPS): RPS {
+    return when (this.uppercase()) {
+        "X" -> when (oppenent) { // lose
+            RPS.Rock -> RPS.Scissors
+            RPS.Paper -> RPS.Rock
+            RPS.Scissors -> RPS.Paper
+        }
+        "Y" -> oppenent // tie
+        "Z" -> when (oppenent) { // win
+            RPS.Rock -> RPS.Paper
+            RPS.Paper -> RPS.Scissors
+            RPS.Scissors -> RPS.Rock
+        }
+        else -> error("Invalid input <$this>")
     }
 }
 
