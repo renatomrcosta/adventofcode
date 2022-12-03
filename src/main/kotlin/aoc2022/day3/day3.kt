10cb5b937ffffefae78f7afbd057ab4e94f10404
Renato Costa <renato@closelink.net>
2022-12-03 09:29:14 +0100

    2022-03 - part2

--- src/main/kotlin/aoc2022/day3/day3.kt
+++ src/main/kotlin/aoc2022/day3/day3.kt
@@ -16,20 +16,32 @@ fun main() {
     val input = readFile("day3.txt")
     part1(testInput).run { require(this == 157) }
     part1(input).run { println("Part1: $this") }
+
+    //
+    part2(testInput).run { require(this == 70)}
+    part2(input).run { println("Part2: $this") }
 }
 
 private fun part1(input: String): Int {
-    return input.parseInput()
+    return input.splitOnLineBreaks()
+        .map {
+            val size = it.length - 1
+            it.toCharArray(0, (size / 2) + 1) to it.toCharArray((size / 2) + 1, size + 1)
+        }
+        .map { (c1, c2) -> c1.toList() to c2.toList() }
         .map { (c1, c2) -> c1 intersect c2 }
         .sumOf { it.first().priority }
 }
 
+private fun part2(input: String): Int = input.splitOnLineBreaks()
+    .windowed(3, step =3)
+    .onEach { println(it) }
+    .map { (c1, c2, c3) ->
+        c1.toList() intersect c2.toList() intersect c3.toList()
+    }
+    .onEach { println(it) }
+    .sumOf { it.first().priority }
+
 private val letters = ('a'..'z') + ('A'..'Z')
 private val Char.priority: Int get() = letters.indexOf(this) + 1 // don't care to error treat index not found
 
-private fun String.parseInput() = this.splitOnLineBreaks()
-    .map {
-        val size = it.length - 1
-        it.toCharArray(0, (size / 2) + 1) to it.toCharArray((size / 2) + 1, size + 1)
-    }
-    .map { (c1, c2) -> c1.toList() to c2.toList() }
