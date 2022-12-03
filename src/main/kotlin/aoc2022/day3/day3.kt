65cdd26f459cb150ba931833a149e76fc3dd9133
Renato Costa <renato@closelink.net>
2022-12-03 09:34:13 +0100

    2022-03 - refactor indices

--- src/main/kotlin/aoc2022/day3/day3.kt
+++ src/main/kotlin/aoc2022/day3/day3.kt
@@ -18,28 +18,21 @@ fun main() {
     part1(input).run { println("Part1: $this") }
 
     //
-    part2(testInput).run { require(this == 70)}
+    part2(testInput).run { require(this == 70) }
     part2(input).run { println("Part2: $this") }
 }
 
 private fun part1(input: String): Int {
     return input.splitOnLineBreaks()
-        .map {
-            val size = it.length - 1
-            it.toCharArray(0, (size / 2) + 1) to it.toCharArray((size / 2) + 1, size + 1)
-        }
-        .map { (c1, c2) -> c1.toList() to c2.toList() }
+        .flatMap { it.toList().chunked(it.length / 2) }
+        .windowed(size = 2, step = 2)
         .map { (c1, c2) -> c1 intersect c2 }
         .sumOf { it.first().priority }
 }
 
 private fun part2(input: String): Int = input.splitOnLineBreaks()
-    .windowed(3, step =3)
-    .onEach { println(it) }
-    .map { (c1, c2, c3) ->
-        c1.toList() intersect c2.toList() intersect c3.toList()
-    }
-    .onEach { println(it) }
+    .windowed(3, step = 3)
+    .map { (c1, c2, c3) -> c1.toList() intersect c2.toList() intersect c3.toList() }
     .sumOf { it.first().priority }
 
 private val letters = ('a'..'z') + ('A'..'Z')
