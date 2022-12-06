3a5cf140f77153ed7a9b07c04f46e4006ededfaf
Renato Costa <renato@closelink.net>
2022-12-06 09:30:36 +0100

    2022-06 - part2

--- src/main/kotlin/aoc2022/day6/day6.kt
+++ src/main/kotlin/aoc2022/day6/day6.kt
@@ -3,24 +3,28 @@ package aoc2022.day6
 import aoc2022.readFile
 
 private val tests = listOf(
-    Triple("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 0),
-    Triple("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 0),
-    Triple("nppdvjthqldpwncqszvftbrmjlhg", 6, 0),
-    Triple("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 0),
-    Triple("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 0),
+    Triple("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 19),
+    Triple("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 23),
+    Triple("nppdvjthqldpwncqszvftbrmjlhg", 6, 23),
+    Triple("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 29),
+    Triple("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 26),
 )
 
 fun main() {
     val input = readFile("day6.txt")
     tests.forEach { (input, expected, _) -> require(part1(input) == expected) }
     part1(input).run { println("Part1: $this") }
+
+    tests.forEach { (input, _, expected) -> require(part2(input) == expected) }
+    part2(input).run { println("Part2: $this") }
 }
 
-private fun part1(input: String): Int {
-    val indexOfPacketStart = input.parseInput().indexOfFirst { group ->
-        group.toSet().size == 4
-    }
-    return indexOfPacketStart + 4
+private fun part1(input: String): Int = input.findIndexOfFirstCompleteUniqueGroup(groupSize = 4)
+
+private fun part2(input: String): Int = input.findIndexOfFirstCompleteUniqueGroup(groupSize = 14)
+
+private fun String.findIndexOfFirstCompleteUniqueGroup(groupSize: Int): Int {
+    return groupSize + this.windowed(size = groupSize, step = 1)
+        .indexOfFirst { group -> group.toSet().size == groupSize }
 }
 
-private fun String.parseInput() = this.windowed(size = 4, step = 1)
