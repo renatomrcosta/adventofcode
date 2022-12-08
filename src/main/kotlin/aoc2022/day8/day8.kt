19d03ac6612f204a77923f66314c648a034043b3
Renato Costa <renato@closelink.net>
2022-12-08 14:51:38 +0100

    2022-08 - part2

--- src/main/kotlin/aoc2022/day8/day8.kt
+++ src/main/kotlin/aoc2022/day8/day8.kt
@@ -14,7 +14,10 @@ private val testData = """
 fun main() {
     val input = readFile("day8.txt")
     part1(testData).run { require(this == 21) { println("result was $this") } }
-    part1(input).run {  println("Part1: $this") }
+    part1(input).run { println("Part1: $this") }
+
+    part2(testData).run { require(this == 8) { println("oops $this")} }
+    part2(input).run { println("Part2: $this") }
 }
 
 fun part1(input: String): Int {
@@ -24,6 +27,45 @@ fun part1(input: String): Int {
     return matrix.mapIndexed { x, row ->
         row.indices.map { y -> (x to y).isVisible(matrix) }
     }.flatten().count { it }
+}
+
+fun part2(input: String): Int {
+    val matrix = input.parseInput()
+
+    return matrix.mapIndexed { x, row ->
+        row.indices.map { y -> (x to y).scenicScore(matrix) }
+    }.flatten().max()
+}
+
+private fun Pair<Int, Int>.scenicScore(matrix: Matrix): Int {
+    val (x, y) = this
+    val currentTreeHeight = matrix[x][y]
+
+    when {
+        // ignore edges
+        x == 0 || y == 0 ||
+            y == matrix.lastIndex ||
+            x == matrix.first().lastIndex -> return 0
+    }
+
+    val ranges = listOf(
+        Triple("left", "cell", (y - 1 downTo 0)), // from cell go left in row
+        Triple("right", "cell", (y + 1..matrix.lastIndex)), // from cell to right in row
+        Triple("up", "row", (x - 1 downTo 0)), // from row go up
+        Triple("down", "row", (x + 1..matrix.first().lastIndex)), // from row go down
+    )
+
+    return ranges.map { (direction, type, range) ->
+        // println("[$x|$y] [$currentTreeHeight] = verifying direction $direction")
+        var score = 0
+        for(index in range) {
+            val item = if (type == "cell") matrix[x][index] else matrix[index][y]
+            score++
+            if(item >= currentTreeHeight) break
+        }
+        // println("[$x|$y] [$currentTreeHeight] = $direction result $score")
+        score
+    }.reduce { acc, score ->  acc * score}
 }
 
 private fun Pair<Int, Int>.isVisible(matrix: Matrix): Boolean {
