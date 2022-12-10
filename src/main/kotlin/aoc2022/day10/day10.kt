3aa921cbc67b39d8dd2462d5174de3be1ae414cb
Renato Costa <renato@closelink.net>
2022-12-10 09:41:07 +0100

    2022-10 - Part1

---
+++ src/main/kotlin/aoc2022/day10/day10.kt
@@ -0,0 +1,74 @@
+package aoc2022.day10
+
+import aoc2022.readFile
+import aoc2022.splitOnLineBreaks
+
+fun main() {
+    val testData = readFile("day10-test.txt")
+    val input = readFile("day10.txt")
+    part1(testData).run { require(this == 13140L) { println("Part1 error: $this") } }
+    part1(input).run { println("Part1: $this") }
+}
+
+private fun part1(input: String): Long {
+    val cpu = CPU()
+    val signalIndexes = listOf(20L, 60L, 100L, 140L, 180L, 220L)
+
+    input.parse().forEach { cpu.operate(it) }
+
+    return signalIndexes.fold(0L) { acc, index ->
+        val xAtLog = cpu.stateLog[index.toInt()] ?: error("what? $index")
+        (xAtLog * index) + acc
+    }
+}
+
+private data class CPU(
+    var x: Long = 1L,
+    var cycle: Int = 1,
+) {
+    val stateLog: MutableMap<Int, Long> = mutableMapOf(cycle to x)
+
+    fun operate(operation: Operation) {
+        when (operation) {
+            is Operation.ADDX, is Operation.NOOP -> {
+                (1..operation.cycleLength).forEach {
+                    stateLog[cycle] = x
+
+                    if (it == operation.cycleLength) {
+                        x += operation.accumulator
+                    }
+
+                    cycle++
+                }
+            }
+        }
+    }
+}
+
+private sealed class Operation(
+    val name: String,
+    open val accumulator: Long,
+    val cycleLength: Int,
+) {
+    object NOOP : Operation(name = "noop", accumulator = 0, cycleLength = 1)
+    data class ADDX(override val accumulator: Long) :
+        Operation(name = "addx", accumulator = accumulator, cycleLength = 2)
+}
+
+private fun String.parse() = this.splitOnLineBreaks()
+    .map {
+        val split = it.split(" ")
+        val cmdName = split.first()
+        val cmdX = (split.getOrNull(1))?.toLong()
+        when (cmdName) {
+            "addx" -> Operation.ADDX(accumulator = cmdX ?: 0L)
+            "noop" -> Operation.NOOP
+            else -> error("invalid command")
+        }
+    }
+
+private val smallTest = """
+    noop
+    addx 3
+    addx -5
+""".trimIndent()
