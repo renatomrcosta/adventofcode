d0838e44ee37a875eeae942a722a4d4ba0113374
Renato Costa <renato@closelink.net>
2023-12-01 10:14:47 +0100

    AoC 2023 - Day 1 part 1

--- src/main/kotlin/aoc2023/day01/day01.kt
+++ src/main/kotlin/aoc2023/day01/day01.kt
@@ -1,7 +1,28 @@
 package aoc2023.day01
 
 import aoc2023.readFile
+import aoc2023.splitOnLineBreaks
+
+private val testInput = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
+""".trimIndent()
 
 fun main() {
-    println(readFile("day1.txt"))
+    part1(testInput).run { require(this == 142L) }
+    part1(readFile("day1.txt")).run { println("Part 1: $this") }
+}
+
+private fun part1(input: String): Long {
+    return input.splitOnLineBreaks()
+        .map { line ->
+            val first = line.first { it.isDigit() }
+            val last = line.last { it.isDigit() }
+
+            "$first$last".toLong()
+        }
+        .onEach { println(it) }
+        .sum()
 }
