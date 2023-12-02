f63d2d05877fa67302a9d2d7e8827d199f3a5a5b
Renato Costa <renato@closelink.net>
2023-12-02 11:13:28 +0100

    AoC 2023 Day 02 Part 1

--- src/main/kotlin/aoc2023/day02/day02.kt
+++ src/main/kotlin/aoc2023/day02/day02.kt
@@ -1,15 +1,66 @@
 package aoc2023.day02
 
 import aoc2023.readFile
+import aoc2023.splitOnLineBreaks
 
 private val testInput = """
-    
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 """.trimIndent()
 
 fun main() {
-    part1(testInput).run { require(this == "") }
+    part1(testInput).run { require(this == 8L) }
     part1(readFile("day2.txt")).run { println("Part 1: $this") }
 }
 
-private fun part1(input: String) = "TODO()"
+private fun part1(input: String): Long {
+    val limits = mapOf(
+        "red" to 12L,
+        "green" to 13L,
+        "blue" to 14L,
+    )
+    val gameSets = buildGameSets(input)
+    println(gameSets)
+
+    return gameSets
+        .filterValues { set -> set.isValid(limits) }
+        .keys
+        .sum()
+}
+
+private fun List<Map<String, Long>>.isValid(limits: Map<String, Long>): Boolean =
+    this.all {gameRound ->
+        limits.entries.all { (color, limit) ->
+            val gameValue = gameRound[color] ?: 0L
+            gameValue <= limit
+        }
+    }
+
+
+private fun buildGameSets(input: String) = input.splitOnLineBreaks()
+    .map { line ->
+        val (gameDefinition, setDefinition) = line.split(":")
+        val gameId = gameDefinition.split(" ").last().toLong()
+        val sets = setDefinition.split(";")
+            .map { it.buildGameMapping() }
+
+        gameId to sets
+    }
+    .toMap()
+
+/**
+ * Takes a set of instructions, spits out a defined map
+ */
+private fun String.buildGameMapping(): Map<String, Long> {
+    return this.split(",")
+        .map {
+            val (number, color) = it.trim().split(" ")
+            color to number.toLong()
+        }
+        .groupBy { it.first }
+        .mapValues { it.value.sumOf { it.second } }
+        .toMap()
 }
