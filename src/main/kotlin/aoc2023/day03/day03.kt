110f5e421b7b2d126ac03143d1b35872828627ee
Renato Costa <renato@closelink.net>
2023-12-03 07:57:48 +0100

    day 03 partial

--- src/main/kotlin/aoc2023/day03/day03.kt
+++ src/main/kotlin/aoc2023/day03/day03.kt
@@ -1,17 +1,40 @@
 package aoc2023.day03
 
-import aoc2023.readFile
+import aoc2023.splitOnLineBreaks
 
 private val testInput = """
-    
+    467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...${'$'}.*....
+    .664.598..
 """.trimIndent()
 
 fun main() {
-    part1(testInput).run { require(this == 8L) }
+    part1(testInput).run { require(this == 4361L) }
 //    part1(readFile("day2.txt")).run { println("Part 1: $this") }
 
 //    part2(testInput).run { require(this == 2286L) }
 //    part2(readFile("day2.txt")).run { println("Part 2: $this") }
 }
 
-private fun part1(input: String) = TODO()
+private fun part1(input: String): Long {
+    val digitRegex = Regex("\\D+")
+    // build a map of numbers vs indices
+    input.splitOnLineBreaks().mapIndexed { row, line ->
+        digitRegex.split(line)
+            .filter { it.isNotEmpty() }
+            // TODO why are empty lists going here?
+            .map { number ->
+                val index = line.indexOf(number)
+                (row to number) to (index..<index + number.length)
+            }
+    }.toList()
+        .also { println(it) }
+    return 0L
+}
