5cbd8726c490f99ede1d6b255693b270a307c4a7
Renato Costa <renato@closelink.net>
2023-12-03 07:38:10 +0100

    AoC 2023 Day 03 prep

---
+++ src/main/kotlin/aoc2023/day03/day03.kt
@@ -0,0 +1,17 @@
+package aoc2023.day03
+
+import aoc2023.readFile
+
+private val testInput = """
+    
+""".trimIndent()
+
+fun main() {
+    part1(testInput).run { require(this == 8L) }
+//    part1(readFile("day2.txt")).run { println("Part 1: $this") }
+
+//    part2(testInput).run { require(this == 2286L) }
+//    part2(readFile("day2.txt")).run { println("Part 2: $this") }
+}
+
+private fun part1(input: String) = TODO()
