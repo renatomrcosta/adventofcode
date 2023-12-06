cd096f1633bf68c7500ee6971326db5103acaae7
Renato Costa <renato@closelink.net>
2023-12-06 17:00:45 +0100

    AoC 2023 Day 04 part1

---
+++ src/main/kotlin/aoc2023/day04/day04.kt
@@ -0,0 +1,44 @@
+package aoc2023.day04
+
+import aoc2023.readFile
+import aoc2023.splitOnLineBreaks
+import kotlin.math.pow
+
+private val testInput = """
+    Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+""".trimIndent()
+
+fun main() {
+    part1(testInput).run { require(this == 13L) }
+    part1(readFile("day4.txt")).run { println("Part1: $this") }
+}
+
+private fun part1(input: String): Long {
+    return input.splitOnLineBreaks()
+        .map { line ->
+            val (_, numbers) = line.split(":")
+            val (winners, drawn) = numbers.split("|")
+            winners.splitNumbers() to drawn.splitNumbers()
+        }
+//        .also { println(it.toList()) }
+        .map { (winners, drawn) ->
+            when (val matches = drawn.intersect(winners.toSet()).size) {
+                0 -> 0.0
+                else -> 2.0.pow(matches - 1.0)
+            }
+        }
+//        .also { println(it.toList()) }
+        .sum()
+        .toLong()
+}
+
+private fun String.splitNumbers() = this.splitToSequence(" ")
+    .map { it.trim() }
+    .filter { it.isNotEmpty() }
+    .map { it.toInt() }
+    .toList()
