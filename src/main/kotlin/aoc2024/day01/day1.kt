ecd2d5630568e9cf6d73590817449831a29c08f9
Renato Costa <renato.rezende.costa@gmail.com>
2024-12-07 21:44:50 +0100

    2024 Day 1 Part2

--- src/main/kotlin/aoc2024/day01/day1.kt
+++ src/main/kotlin/aoc2024/day01/day1.kt
@@ -13,7 +13,8 @@ private val testData = """
 3   3
 """.trimIndent()
 
-private const val TEST_EXPECTED_RESULT = 11L
+private const val TEST_EXPECTED_RESULT_PART1 = 11L
+private const val TEST_EXPECTED_RESULT_PART2 = 31L
 
 private val NUM_REGEX = Regex("[0-9]+")
 
@@ -27,18 +28,31 @@ fun String.parse(): Pair<List<Long>, List<Long>> {
             left.add(num1.toLong())
             right.add(num2.toLong())
         }
-    return left.sorted() to right.sorted()
+    return left to right
 }
 
 fun part1(input: String): Long {
     val (left, right) = input.parse()
-    val paired = left.zip(right)
+    val paired = left.sorted().zip(right.sorted())
     val distanceList = paired.map { (l, r) -> abs(l - r) }
     return distanceList.sum()
 }
 
+fun part2(input: String): Long {
+    val (left, right) = input.parse()
+    val valueSimilarityMap = left.map {
+        it to right.filter { r -> r == it }.count()
+    }
+//    println(valueSimilarityMap)
+    return valueSimilarityMap.map { (v, sm) -> v * sm }.sum()
+}
+
 fun main() {
     val input = readFile("day01.txt")
-    part1(testData).run { require(this == TEST_EXPECTED_RESULT) }
+    part1(testData).run { require(this == TEST_EXPECTED_RESULT_PART1) }
     part1(input).run { println("Part1: $this") }
+
+    part2(testData).run{ require(this == TEST_EXPECTED_RESULT_PART2) }
+    part2(input).run { println("Part2: $this") }
+
 }
