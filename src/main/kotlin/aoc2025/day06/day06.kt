13de3d42e67552da828a762b4342ef5b40544bc1
Renato Costa <renato.rezende.costa@gmail.com>
2025-12-06 19:44:26 +0100

    2025 Day 6 Part2

--- src/main/kotlin/aoc2025/day06/day06.kt
+++ src/main/kotlin/aoc2025/day06/day06.kt
@@ -1,6 +1,5 @@
 package aoc2025.day06
 
-import aoc2025.println
 import aoc2025.readFile
 import aoc2025.splitOnLineBreaks
 
@@ -16,6 +15,9 @@ fun main() {
 
     part1(testData).run { require(this == 4277556L) { "Invalid result $this" } }
     part1(input).run { println("Part1: $this") }
+
+    part2(testData).run { require(this == 3263827L) { "Invalid result $this" } }
+    part2(input).run { println("Part2: $this") }
 }
 
 private fun part1(input: String): Long {
@@ -31,7 +33,50 @@ private fun part1(input: String): Long {
 }
 
 
-private fun part2(input: String): Long = TODO()
+data class OperationGroup(
+    val operation: String,
+    val numbers: List<Long>,
+)
+
+private fun part2(input: String): Long {
+    val parsed = input.parseInputP2()
+    var acc = mutableListOf<MutableList<String>>()
+    val grouped = mutableListOf<OperationGroup>()
+    parsed.forEach { group ->
+        if (group.all { it.isBlank() }) {
+            if (acc.isNotEmpty()) {
+                val operation = acc.last().last()
+                val groups = acc.eraseOperation()
+                    .map { it.joinToString("").trim() }
+                    .filter { it.isNotBlank() }
+//                .also { println(it) }
+                    .map { it.toLong() }
+
+                grouped.add(OperationGroup(operation = operation, numbers = groups))
+                acc = mutableListOf()
+            }
+        } else {
+            acc.add(group.toMutableList())
+        }
+    }
+
+    return grouped.sumOf { (operation, nums) ->
+        when (operation) {
+            "*" -> nums.reduce { a, b -> a * b }
+            "+" -> nums.reduce { a, b -> a + b }
+            else -> error("Invalid operation $operation")
+        }
+    }
+}
+
+private fun MutableList<MutableList<String>>.eraseOperation() = map { list ->
+    list.map { char ->
+        when (char) {
+            "*", "+" -> ""
+            else -> char
+        }
+    }
+}
 
 
 @Suppress("UNCHECKED_CAST")
@@ -42,14 +87,21 @@ private fun String.parseInput(): List<List<String>> {
         .transpose() as List<List<String>>
 }
 
-private fun <T> List<List<T>>.transpose(): List<List<T?>> {
+@Suppress("UNCHECKED_CAST")
+private fun String.parseInputP2(): List<List<String>> = splitOnLineBreaks()
+    .map { line -> line.reversed().split("") }
+    .toList()
+    .transpose(substitute = "") as List<List<String>>
+
+private fun <T> List<List<T>>.transpose(substitute: T? = null): List<List<T?>> {
     val rowSize = this.size
     val colSize = this.first().size
     val transposed = MutableList(colSize) { MutableList<T?>(rowSize) { null } }
 
     for (i in 0 until rowSize) {
         for (j in 0 until colSize) {
-            transposed[j][i] = this[i][j]
+//            transposed[j][i] = this[i][j]
+            transposed[j][i] = this.getOrNull(i)?.getOrNull(j) ?: substitute
         }
     }
     return transposed
